c6f83a465f525f186d8da555237bd4eda51a8e22
ArtemKhairov <artemxairoff@yandex.ru>
2021-11-05 01:16:18 +0700

    try graphql apollo

---
+++ server/models/schema.js
@@ -0,0 +1,37 @@
+const {buildSchema}=require('graphql')
+const schema=buildSchema(`
+  type User{
+    id:ID
+    username: String
+    age: Int
+    posts: [Post]
+  }
+  type Post{
+    id:ID
+    title:String
+    Content:String
+  }
+
+  input UserInput{
+    id:ID
+    username:String!
+    age:Int!
+    posts:[PostInput]
+  }
+  input PostInput{
+    id:ID
+    title:String!
+    Content:String!
+  }
+
+  type Query{
+    getAllUsers: [User]
+    getUser(id:ID):User
+  }
+
+  type Mutation{
+    createUser(input:UserInput):User
+  }
+`)
+
+module.exports=schema
